0294ed093247af25e4038607e662cdbe3ccd6ae5
Daniel <castronuevodanielalejandro@gmail.com>
2025-10-28 22:15:05 -0300

    Volvemos a usar la ruta absoluta en el NODESTART.sh

--- NODESTART.sh
+++ NODESTART.sh
@@ -9,13 +9,6 @@ RESET="\e[0m"
 BASE_DIR="$(dirname "$0")"
 cd "$BASE_DIR" || { echo -e "${RED}[!] Error:${RESET} No se pudo entrar a $BASE_DIR"; exit 1; }
 
-NODE_BIN=$(which node)
-
-if [ -z "$NODE_BIN" ]; then
-  echo -e "${RED}[!] Error:${RESET} Node.js no encontrado en el PATH."
-  exit 1
-fi
-
-echo -e "${YELLOW}[+] Ejecutando server.js con ${NODE_BIN}${RESET}"
-"$NODE_BIN" server.js
-
+# Ejecutar server.js directamente
+echo -e "${YELLOW}[+] Ejecutando server.js...${RESET}"
+/home/MARCADOR/.nvm/versions/node/v24.5.0/bin/node server.js
